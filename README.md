acfa2bf5e41d9ae40440cd14d7f85a1d247826e4
Ganesh Chincholkar <ganesh.chincholkar@siemens.com>
2023-12-27 23:32:59 +0530

    Homepage with upload and list routes in response

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # node-file-server-no-library
 
 This is simple file server created using node. The idea is to do it with basic http package provided by the node. Without the use of libraries such as express.
+
+#### Server Capabilities
+* Uploade File
+* Download File
+* Check if a file is present on the server
--- index.js
+++ index.js
@@ -1,12 +1,62 @@
-const http = require('http');
+const http = require("http");
 
-const requestListener = (req, res) => {
+const port = 8000;
+
+const homepageRequest = (req, res) => {
     res.writeHead(200, {
-        myHeader: "my value"
+        "content-type": "text/html",
     });
-    res.end("My raw server");
-}
+    res.end(`
+            <!DOCTYPE html>
+                <head>
+                    <title>File Server</title>
+                </head>
+                <style>
+                    *,
+                    html {
+                        margin: 0;
+                        padding: 0;
+                        border: 0;
+                    }
 
-const server = http.createServer(requestListener)
+                    html {
+                        width: 100%;
+                        height: 100%;
+                    }
 
-server.listen(8000);
+                    body {
+                        width: 100%;
+                        height: 100%;
+                        position: relative;
+                    }
+
+                    .center {
+                        width: 100%;
+                        height: 50%;
+                        margin: 0;
+                        position: absolute;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -50%);
+                        text-align: center;
+                    }
+                </style>
+                <body>
+                    <div class="center">
+                        <h1>Filer Server</h1>
+						<ul>
+							<li><a href="/upload">Upload File</a></li>
+							<li><a href="/list">List Files</a></li>
+						</ul>
+                    </div>
+                </body>
+
+            </html>
+    `);
+};
+
+const server = http.createServer(homepageRequest);
+
+server.listen(port, () => {
+    console.log(`Server running on: http://localhost:${port}`);
+});
