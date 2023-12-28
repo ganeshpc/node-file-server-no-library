2bcfb42425561c180b96ba0f2c4186854d607032
Ganesh Chincholkar <ganesh.chincholkar@siemens.com>
2023-12-28 22:12:42 +0530

    Added midleware in routes to extract url parameters from the url

--- src/router.js
+++ src/router.js
@@ -26,22 +26,19 @@ Router.prototype.delete = function (url, handler) {
 };
 
 Router.prototype.handle = function (req, res) {
+    req.queryParams = getUrlParams(req.url);
+    req.url = req.url.replace(/\?.*/, '');
     const url = req.url;
+
+
     const method = req.method;
     let listener;
-    
+
     switch (method) {
-        case 'GET':
-            listener = this.getListeners.find(listener => listener.url === url);
-            if (listener) {
-                listener.handler(req, res);
-            } else {
-                this.errorListener(req, res);
-            }
-            break;
-    
-        case 'POST':
-            listener = this.postListeners.find(listener => listener.url === url);
+        case "GET":
+            listener = this.getListeners.find(
+                (listener) => listener.url === url
+            );
             if (listener) {
                 listener.handler(req, res);
             } else {
@@ -49,8 +46,21 @@ Router.prototype.handle = function (req, res) {
             }
             break;
 
-        case 'DELETE':
-            listener = this.deleteListeners.find(listener => listener.url === url);
+        case "POST":
+            listener = this.postListeners.find(
+                (listener) => listener.url === url
+            );
+            if (listener) {
+                listener.handler(req, res);
+            } else {
+                this.errorListener(req, res);
+            }
+            break;
+
+        case "DELETE":
+            listener = this.deleteListeners.find(
+                (listener) => listener.url === url
+            );
             if (listener) {
                 listener.handler(req, res);
             } else {
@@ -62,11 +72,20 @@ Router.prototype.handle = function (req, res) {
             this.errorListener(req, res);
             break;
     }
-}
+};
 
 Router.prototype.errorListener = function (req, res) {
     res.writeHead(200);
     res.end(`<h1> Not found </h1>`);
+};
+
+function getUrlParams(url) {
+    const params = {};
+    const pattern = /[?&]+([^=&]+)=([^&]*)/gi;
+    url.replace(pattern, function (match, key, value) {
+        params[key] = decodeURIComponent(value);
+    });
+    return params;
 }
 
 module.exports = Router;
